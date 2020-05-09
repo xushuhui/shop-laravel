50c0ac3e7567951d398a394abe85c73cc8c6c777
xushuhui <474497097@qq.com>
2020-05-09 09:56:50 +0800

    feat:加入购物车

--- app/Http/Requests/UserAddressRequest.php
+++ app/Http/Requests/UserAddressRequest.php
@@ -5,16 +5,8 @@ namespace App\Http\Requests;
 
 class UserAddressRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
-
+    
+    
     /**
      * Get the validation rules that apply to the request.
      *
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+    
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-
     protected $fillable = [
         'name', 'email', 'password', 'email_verified',
     ];
-
+    
     protected $casts = [
         'email_verified' => 'boolean',
     ];
@@ -30,8 +29,21 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
     public function addresses()
     {
         return $this->hasMany(UserAddress::class);
+    }
+    
+    public function favoriteProducts()
+    {
+        return $this->belongsToMany(Product::class, 'user_favorite_products')
+            ->withTimestamps()
+            ->orderBy('user_favorite_products.created_at', 'desc');
+    }
+    
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
     }
 }
